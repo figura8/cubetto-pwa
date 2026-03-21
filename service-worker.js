f68dbb011b8303244edbee0df04221f1850256d8
figura8 <figuraotto@gmail.com>
2026-03-21 22:32:57 +0100

    Update app icons and remove unused assets

--- service-worker.js
+++ service-worker.js
@@ -10,8 +10,7 @@ const PRECACHE_URLS = [
   "./data/editor-levels.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  "./icons/boks-apple-touch-icon.png",
-  "./icons/favicon.png"
+  "./icons/boks-apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
